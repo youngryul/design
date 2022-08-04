3e026878bd08021f610d25d9932aeca094e2d1f3
PNS212624\Hansol <yrlee00@hansol.com>
2022-08-04 15:02:35 +0900

    8/4 commit

---
+++ src/com/company/design/singleton/SocketClient.java
@@ -0,0 +1,24 @@
+package com.company.design.singleton;
+
+public class SocketClient {
+
+    private static SocketClient socketClient = null;
+
+    public SocketClient(){
+
+    }
+
+    public static SocketClient getInstance(){
+
+        if(socketClient == null){
+            socketClient = new SocketClient();
+
+        }
+        return socketClient;
+    }
+
+    public void connet(){
+        System.out.println("coonet");
+    }
+
+}
